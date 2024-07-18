0a96b34b620ea0fe19aa20dffebb947e1bf4ea05
ksg0605 <ksg0605@gmail.com>
2024-07-18 10:08:08 +0900

    feature: add GratitudeViewModel

--- DailyGratitudeApp/ViewModels/GratitudeViewModel.swift
+++ DailyGratitudeApp/ViewModels/GratitudeViewModel.swift
@@ -6,3 +6,33 @@
 //
 
 import Foundation
+
+class GratitudeViewModel: ObservableObject {
+    @Published var notes: [GratitudeNote] = []
+    
+    func addNote(note: String) {
+        let newNote = GratitudeNote(date: Date(), note: note)
+        notes.append(newNote)
+        saveNotes()
+    }
+    
+    func saveNotes() {
+        do {
+            let data = try JSONEncoder().encode(notes)
+            UserDefaults.standard.set(data, forKey: "gratitudeNotes")
+        } catch {
+            print("저장 실패")
+        }
+    }
+    
+    func loadNotes() {
+        if let data = UserDefaults.standard.data(forKey: "gratitudeNotes"),
+           let savedNotes = try? JSONDecoder().decode([GratitudeNote].self, from: data) {
+            notes = savedNotes
+        }
+    }
+    
+    init() {
+        loadNotes()
+    }
+}
