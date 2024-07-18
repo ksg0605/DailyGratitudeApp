cc74c90a49ad66be03f12b063ece05942aeabced
ksg0605 <ksg0605@gmail.com>
2024-07-18 09:07:54 +0900

    Initial Commit

---
+++ DailyGratitudeApp/DailyGratitudeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DailyGratitudeAppApp.swift
+//  DailyGratitudeApp
+//
+//  Created by 김선규 on 7/18/24.
+//
+
+import SwiftUI
+
+@main
+struct DailyGratitudeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
